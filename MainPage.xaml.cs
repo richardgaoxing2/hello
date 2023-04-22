80b9775894d828ba459f5606f84b449991e242b3
GAOXING <131503547+richardgaoxing2@users.noreply.github.com>
2023-04-22 10:13:09 +0800

    hello

---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloNewWorld;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+}
+
---
+++ ServiceLocator.cs
@@ -0,0 +1,18 @@
+﻿using HelloNewWorld.ViewModels;
+
+namespace HelloNewWorld
+{
+    public  class ServiceLocator
+    {
+        private IServiceProvider _serviceProvider;
+        public MainPageViewModel MainPageViewModel => _serviceProvider.GetService<MainPageViewModel>();//依赖注入 IoC
+        public ServiceLocator()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<MainPageViewModel>();
+           // serviceCollection.AddSingleton<IKeyValueSrorage, KeyValueStorage>();
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+    }
+
+}
